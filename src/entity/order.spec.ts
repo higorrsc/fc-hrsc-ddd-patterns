62733292c6ed2ab12abff2703db0bd9da77eeea3
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-09-24 20:32:26 -0300

    feat: add order tests

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,34 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit tests", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      let order = new Order("", "123", []);
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw error when customerid is empty", () => {
+    expect(() => {
+      let order = new Order("1", "", []);
+    }).toThrowError("CustomerId is required");
+  });
+
+  it("should throw error when items are empty", () => {
+    expect(() => {
+      let order = new Order("1", "1", []);
+    }).toThrowError("Items are required");
+  });
+
+  it("should calculate total", () => {
+    const order1 = new Order("1", "1", [new OrderItem("1", "Item 1", 10, 1)]);
+    expect(order1.total()).toBe(10);
+
+    const order2 = new Order("1", "1", [
+      new OrderItem("1", "Item 1", 10, 1),
+      new OrderItem("2", "Item 2", 20, 1),
+      new OrderItem("3", "Item 3", 30, 3),
+    ]);
+    expect(order2.total()).toBe(120);
+  });
+});
