bb10aab68d47e70c8180b4fe37506a797ddfaded
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-10-07 19:31:13 -0300

    feat: implement order repository update, find and find all functions

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../domain/entity/order";
+import OrderItem from "../../domain/entity/order_item";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
@@ -21,5 +22,111 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel, as: "items" }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    await OrderModel.update(
+      {
+        customer_id: entity.customerId,
+        total: entity.total(),
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
+
+    const existingItems = await OrderItemModel.findAll({
+      where: {
+        order_id: entity.id,
+      },
+    });
+
+    for (const item of entity.items) {
+      const existingItem = existingItems.find((i) => i.id === item.id);
+      if (existingItem) {
+        await OrderItemModel.update(
+          {
+            name: item.name,
+            price: item.price,
+            product_id: item.productId,
+            quantity: item.quantity,
+          },
+          {
+            where: {
+              id: item.id,
+            },
+          }
+        );
+      } else {
+        await OrderItemModel.create({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+          order_id: entity.id,
+        });
+      }
+    }
+
+    const deleteItems = existingItems.filter(
+      (existingItem) =>
+        !entity.items.some((item) => item.id === existingItem.id)
+    );
+
+    for (const item of deleteItems) {
+      await OrderItemModel.destroy({
+        where: {
+          id: item.id,
+        },
+      });
+    }
+  }
+
+  async find(id: string): Promise<Order> {
+    let orderModel;
+    let orderItems;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        rejectOnEmpty: true,
+        include: ["items"],
+      });
+      orderItems = orderModel.items.map(
+        (item) =>
+          new OrderItem(
+            item.id,
+            item.product_id,
+            item.name,
+            item.price,
+            item.quantity
+          )
+      );
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+    return new Order(orderModel.id, orderModel.customer_id, orderItems);
+  }
+
+  async findAll(): Promise<Order[]> {
+    const ordersModel = await OrderModel.findAll({
+      include: ["items"],
+    });
+    return ordersModel.map((orderModel) => {
+      const orderItems = orderModel.items.map((item) => {
+        return new OrderItem(
+          item.id,
+          item.product_id,
+          item.name,
+          item.price,
+          item.quantity
+        );
+      });
+      return new Order(orderModel.id, orderModel.customer_id, orderItems);
+    });
   }
 }
