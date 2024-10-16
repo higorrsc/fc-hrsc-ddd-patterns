5787f2573dba19ce08d11406dd8deefff8d63f88
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-10-16 20:53:34 -0300

    feat: implement customer created event handle and test

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,6 @@
+import CustomerCreatedEvent from "../customer/customer-created.event";
+import SendConsoleLog1WhenCustomerIsCreatedHandler from "../customer/handler/send-console-log-1-when-customer-is-created.handler";
+import SendConsoleLog2WhenCustomerIsCreatedHandler from "../customer/handler/send-console-log-2-when-customer-is-created.handler";
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../product/product-created.event";
 import EventDispatcher from "./event-dispatcher";
@@ -65,5 +68,42 @@ describe("Domain events tests", () => {
 
     eventDispatcher.notify(productCreatedEvent);
     expect(spyEventHandler).toHaveBeenCalled();
+  });
+});
+
+describe("Customer domain events unit tests", () => {
+  it("should notify when a customer is created", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendConsoleLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendConsoleLog2WhenCustomerIsCreatedHandler();
+    const eventName = "CustomerCreatedEvent";
+
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    eventDispatcher.register(eventName, eventHandler1);
+
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+    eventDispatcher.register(eventName, eventHandler2);
+
+    expect(eventDispatcher.getEventHandlers[eventName][0]).toMatchObject(
+      eventHandler1
+    );
+    expect(eventDispatcher.getEventHandlers[eventName][1]).toMatchObject(
+      eventHandler2
+    );
+
+    const customerCreateEvent = new CustomerCreatedEvent({
+      id: "1",
+      name: "Customer 1",
+      address: {
+        street: "Street 1",
+        number: 1,
+        zip: "Zip 1",
+        city: "City 1",
+      },
+    });
+
+    eventDispatcher.notify(customerCreateEvent);
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
   });
 });
