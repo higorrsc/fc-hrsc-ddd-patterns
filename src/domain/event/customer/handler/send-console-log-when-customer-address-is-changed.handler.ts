67a80f014520710a237a43b8e55559b43ef71b53
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-10-16 20:57:26 -0300

    feat: implement customer change address event handle and test

---
+++ src/domain/event/customer/handler/send-console-log-when-customer-address-is-changed.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import CustomerAddressChangedEvent from "../customer-address-changed.event";
+
+export default class SendConsoleLogWhenCustomerAddressIsChangedHandler
+  implements EventHandlerInterface
+{
+  handle(event: CustomerAddressChangedEvent): void {
+    console.log("Endereço do cliente: {id}, {nome} alterado para: {endereço}");
+  }
+}
